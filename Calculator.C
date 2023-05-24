0a4153a9956d4c4f60e7969242aecb244f8e345a
Umair Javed (2K22/CSE/113) <115348730+umairrrk24@users.noreply.github.com>
2023-05-24 21:14:47 +0500

    Update Calculator.C
    I have reorganized this code.

--- Calculator.C
+++ Calculator.C
@@ -2,41 +2,42 @@
 
 int main() {
     char operator;
-    double num1, num2, result;
+    double num1, num2, output;
 
-    printf("Enter an operator (+, -, *, /): ");
+    printf("Enter an operator (/, *, +, -): ");
     scanf("%c", &operator);
 
-    printf("Enter two numbers: ");
-    scanf("%lf %lf", &num1, &num2);
+    printf("Enter first number: ");
+    scanf("%lf", &num1);
+    printf("Enter Second Number:");
+    scanf("%lf", &num2);
 
-    switch(operator) {
-        case '+':
-            result = num1 + num2;
-            printf("Result: %.2lf\n", result);
-            break;
-
-        case '-':
-            result = num1 - num2;
-            printf("Result: %.2lf\n", result);
-            break;
-
-        case '*':
-            result = num1 * num2;
-            printf("Result: %.2lf\n", result);
-            break;
-
+    switch (operator) {
         case '/':
-            if(num2 != 0) {
-                result = num1 / num2;
-                printf("Result: %.2lf\n", result);
+            if (num2 != 0) {
+                output = num1 / num2;
+                printf("output: %.2lf\n", output);
             } else {
-                printf("Error: Division by zero\n");
+                printf("Error: Division by zero is not allowed.\n");
             }
             break;
-
+        case '*':
+            output = num1 * num2;
+            printf("output: %.2lf\n", output);
+            break;    
+        case '+':
+            output = num1 + num2;
+            printf("output: %.2lf\n", output);
+            break;
+        case '-':
+            output = num1 - num2;
+            printf("output: %.2lf\n", output);
+            break;
+        
+        
         default:
-            printf("Error: Invalid operator\n");
+            printf("Error: Invalid operator.\n");
+            break;
     }
 
     return 0;
